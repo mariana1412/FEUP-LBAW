a41d2b985f656c1876d049a389c7ff34450a7749
Allan Sousa <allanbp.br@gmail.com>
2021-05-29 15:12:03 +0100

    fixed bud in show comment policy

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -30,12 +30,11 @@ class CommentPolicy
     public static function show(Comment $comment){
         // blocked users cant see posts
         if($comment == null) return false;
-        if(Auth::check()){
-            $blocked_users = DB::table("block_user")->where('blocked_user',$comment->user_id)->where("blocking_user",Auth::user()->id)->count();
-            $blocking_users = DB::table("block_user")->where('blocked_user',Auth::user()->id)->where("blocking_user",$comment->user_id)->count();
-            return $blocked_users == 0 && $blocking_users == 0;
-        }
-        return false;
+        $user_id = Auth::check()?Auth::user()->id:0;
+        $blocked_users = DB::table("block_user")->where('blocked_user',$comment->user_id)->where("blocking_user",$user_id)->count();
+        $blocking_users = DB::table("block_user")->where('blocked_user',$user_id)->where("blocking_user",$comment->user_id)->count();
+        return $blocked_users == 0 && $blocking_users == 0;
+        
     }
     
 
