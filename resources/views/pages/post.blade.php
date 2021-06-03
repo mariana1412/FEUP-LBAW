c3be9f12ccee9530d617f85ce95fbcf0208f24b3
Allan Sousa <allanbp.br@gmail.com>
2021-06-04 00:07:45 +0100

    small changes

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -50,7 +50,7 @@
                     <div class="mt-2 col-10 justify-content-center d-flex">
                         <div class="row thumbnail-image">
                             <img src="{{URL::asset($metadata['thumbnail'])}}"
-                                 class="justify-content-center" alt="Post thumbnail"  >
+                                 class="justify-content-center" alt="Post thumbnail" >
                         </div>
                     </div>
                 </div>
